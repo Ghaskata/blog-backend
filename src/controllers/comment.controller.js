21efbf93ffcf3be0f58e8b4de4f79503d5da5076
Ghaskata <you@example.com>
2024-02-03 22:34:40 +0530

    comment controller bug fix

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -12,21 +12,29 @@ const addComment = asyncHandler(async (req, res) => {
     throw new ApiError(404, "video id ??? or content  missing???");
   }
 
-  const videoExist = await Video.findById(videoId);
+  const videoObjectId = new mongoose.Types.ObjectId(videoId);
+  if (!isValidObjectId(videoObjectId)) {
+    throw new ApiError(400, "invalid video id");
+  }
+
+  const videoExist = await Video.findById(videoObjectId);
   if (!videoExist) {
     throw new ApiError(403, "invalid vedio id vedio not exist");
   }
 
   const newComment = await Comment.create({
     content: content,
-    video: videoExist._id,
+    video: videoObjectId,
     owner: req.user._id,
   });
-  const createdComment = await newComment.findById(newComment._id);
+
+  const createdComment = await Comment.findById(newComment._id);
   if (!createdComment) {
     throw new ApiError(500, "somthing gets wrong white creating comment");
   }
+
   console.log("created comment >>>", createdComment);
+
   return res
     .status(200)
     .json(new ApiResponse(200, createdComment, "commented successfully"));
@@ -39,18 +47,28 @@ const updateComment = asyncHandler(async (req, res) => {
     throw new ApiError(404, "comment id or content  missing ???");
   }
 
-  const commentExist = await Comment.findById(commentId);
+  const commentObjectId = new mongoose.Types.ObjectId(commentId);
+  if (!isValidObjectId(commentObjectId)) {
+    throw new ApiError(400, "invalid comment id");
+  }
+
+  const commentExist = await Comment.findById(commentObjectId);
   if (!commentExist) {
     throw new ApiError(403, "invalid comment id , comment not exist");
   }
 
+  if (commentExist.owner !== req.user._id) {
+    throw new ApiError(402, "comment not made by your you can't update it");
+  }
+
   const updatedComment = await Comment.findByIdAndUpdate(
-    commentExist._id,
+    commentObjectId,
     {
       $set: { content: content },
     },
     { new: true }
   );
+
   if (!updatedComment) {
     throw new ApiError(500, "somthing gets wrong white updateing comment");
   }
@@ -68,12 +86,22 @@ const deleteComment = asyncHandler(async (req, res) => {
     throw new ApiError(404, "comment  id ??? ");
   }
 
-  const commentExist = await Comment.findById(commentId);
+  const commentObjectId = new mongoose.Types.ObjectId(commentId);
+
+  if (!isValidObjectId(commentObjectId)) {
+    throw new ApiError(400, "invalid comment id");
+  }
+
+  const commentExist = await Comment.findById(commentObjectId);
   if (!commentExist) {
     throw new ApiError(403, "comment not exist");
   }
 
-  const deletedComment = await Comment.deleteOne({ _id: commentExist._id });
+  if (commentExist.owner !== req.user._id) {
+    throw new ApiError(402, "comment not made by your you can't delete it");
+  }
+
+  const deletedComment = await Comment.deleteOne({ _id: commentObjectId });
   console.log("deleted comment >>>", deletedComment);
   return res
     .status(200)
@@ -89,7 +117,13 @@ const getVideoComments = asyncHandler(async (req, res) => {
     throw new ApiError(404, "video id not ??? ");
   }
 
-  const videoExist = await Video.findById(videoId);
+  const videoObjectId = new mongoose.Types.ObjectId(videoId);
+
+  if (!isValidObjectId(videoObjectId)) {
+    throw new ApiError(400, "invalid video id");
+  }
+
+  const videoExist = await Video.findById(videoObjectId);
   if (!videoExist) {
     throw new ApiError(403, "invalid vedio id vedio not exist");
   }
@@ -115,7 +149,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //   ]);
 
   const comments = await Comment.find({
-    videoId: mongoose.Types.ObjectId(videoId),
+    videoId: videoObjectId,
   });
 
   console.log("get all comments >>> ", comments);
